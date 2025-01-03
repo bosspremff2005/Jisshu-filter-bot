41809449b1cf2cf3ec7cb107319bf39de7eae47b
Amarsingh008 <amarsinghrj92@gmail.com>
2025-01-03 19:32:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '28144571'))
-API_HASH = environ.get('API_HASH', 829d51afdd48a3c537917f65bf4f0ada'')
+API_HASH = environ.get('API_HASH', '829d51afdd48a3c537917f65bf4f0ada')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
